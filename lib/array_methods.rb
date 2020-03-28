a132cd3d557e5883840c1e058789ae6d70ebe849
Adao Natalino <sansonov@gmail.com>
2020-03-28 14:49:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -26,5 +26,13 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  
+   i = 0
+  min = 0
+  while i < array.length do
+    if array[i] < big
+    big = array[i]
+    end
+    i+=1
+  end
+  return min
 end
