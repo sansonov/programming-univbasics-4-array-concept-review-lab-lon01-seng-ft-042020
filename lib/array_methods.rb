b6e60a3ee4556e53e283a8df044bd6070c99fb40
Adao Natalino <sansonov@gmail.com>
2020-03-28 14:05:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   i = 0
   new = []
-  while (i < array.length) do
+  while array[i] do
     if (value_to_find === array[i]) do
       return i
     end
